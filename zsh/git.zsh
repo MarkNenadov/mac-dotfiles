dbdef3807f870703481cc071aedf735c118d0f04
Mark Nenadov <marknenadov@gmail.com>
2025-02-15 07:22:25 -0500

    Add clean_branches

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -2,3 +2,8 @@ last_commit() {
     ref=$(git symbolic-ref HEAD 2> /dev/null) || return
     git log -1 --pretty=format:"%ar" | sed 's/\([0-9]*\) \(.\).*/\1\2/'
 }
+
+clean_branches() {
+  git remote prune origin
+  git branch -vv | grep "origin/.*: gone]" | awk '{print $1}' | xargs git branch -D
+}
