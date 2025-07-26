5d0c2a5f87331d8b536759123972c4dd46b88836
Mark Nenadov <marknenadov@gmail.com>
2025-07-25 21:59:39 -0400

    improvements to file-system.zsh

--- zsh/file-system.zsh
+++ zsh/file-system.zsh
@@ -1,9 +1,33 @@
 mkcd() {
-  mkdir "$1"
-  cd "$1" || return 1
+    if [ -z "$1" ]; then
+        echo "Usage: mkcd <directory_name>"
+        return 1
+    fi
+    mkdir "$1"
+    cd "$1" || return 1
 }
 
-trash() { mv $1 ~/.Trash }
+trash() {
+    if [ -z "$1" ]; then
+        echo "Usage: trash <file_or_directory>"
+        return 1
+    fi
+    
+    # Prevent trashing critical directories
+    case "$1" in
+        / | /bin | /usr | /etc | /var | /System | /Library | ~ | $HOME)
+            echo "Error: Cannot trash system directory '$1'"
+            return 1
+            ;;
+    esac
+    
+    if [ ! -e "$1" ]; then
+        echo "Error: '$1' does not exist"
+        return 1
+    fi
+    
+    mv "$1" ~/.Trash
+}
 
 f() {
     find . -name "$1*"
