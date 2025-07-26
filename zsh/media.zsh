8637edba8c9c42a6c309bfc12e4c5d86e84eb9ef
Mark Nenadov <marknenadov@gmail.com>
2025-07-26 06:47:22 -0400

    addd no file specied errors to the 2xyz media.zsh functions

--- zsh/media.zsh
+++ zsh/media.zsh
@@ -1,11 +1,23 @@
 function 2heic() {
+  if [ $# -eq 0 ]; then
+    echo "Error: No files specified. Usage: 2heic <file1> [file2 ...]"
+    return 1
+  fi
   for i in "$@"; do sips -s format heic -s formatOptions 80 "$i" --out "${i%.*}.heic"; done
 }
 
 function 2png() {
+  if [ $# -eq 0 ]; then
+    echo "Error: No files specified. Usage: 2png <file1> [file2 ...]"
+    return 1
+  fi
   for i in "$@"; do sips -s format png -s formatOptions 80 "$i" --out "${i%.*}.png"; done
 }
 
 function 2jpg() {
+  if [ $# -eq 0 ]; then
+    echo "Error: No files specified. Usage: 2jpg <file1> [file2 ...]"
+    return 1
+  fi
   for i in "$@"; do sips -s format jpg -s formatOptions 80 "$i" --out "${i%.*}.jpg"; done
 }
