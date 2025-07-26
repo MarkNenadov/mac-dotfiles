e729466eafde21f3ade5bb40369aa882cb8b865f
Mark Nenadov <marknenadov@gmail.com>
2025-07-26 10:07:58 -0400

    Add dryrun testing script/config

---
+++ test-dryrun.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+# This script is used to test the dry run functionality of the install script 
+# (it throws an error if it tries to change things)
+
+/usr/bin/sandbox-exec -f ./test-dryrun.sb ./install.sh --dry-run
