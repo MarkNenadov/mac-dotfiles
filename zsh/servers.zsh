5e6ca2657b4383cb67d5e564f8dd4f18628a6466
Mark Nenadov <marknenadov@gmail.com>
2025-07-26 06:53:11 -0400

    http server function

---
+++ zsh/servers.zsh
@@ -0,0 +1,10 @@
+# Start HTTP server from current directory
+# from https://github.com/cassiascheffer/dotfiles/blob/main/zsh/.zshrc
+
+server() {
+    local port="${1:-8000}"
+    echo "Starting HTTP server on port $port..."
+    echo "Open http://localhost:$port in your browser"
+    sleep 1 && open "http://localhost:$port/" &
+    python3 -m http.server "$port"
+}
