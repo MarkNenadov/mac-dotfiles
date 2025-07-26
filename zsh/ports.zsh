05d775de72881401ea6fa0fd3f1b7a5d39149936
Mark Nenadov <marknenadov@gmail.com>
2025-07-26 07:00:12 -0400

    work on ports.zsh

--- zsh/ports.zsh
+++ zsh/ports.zsh
@@ -1,7 +1,18 @@
+unalias whatison 2>/dev/null
+unalias openports 2>/dev/null
+
 whatison() {
     if [ -z "$1" ]; then
         echo "Usage: whatison <port_number>"
         return 1
     fi
     sudo lsof -i :$1
+}
+
+openports() {
+    if [ -z "$1" ]; then
+        echo "Usage: openports <search string>"
+        return 1
+    fi
+    sudo lsof -i -n -P | grep TCP | grep $1
 }
